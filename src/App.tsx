c0e17d90603b157ba5a0149fa63859975de9b9e1
Utkarsh Trivedi <utkarsh.trivedi@mdlz.com>
2025-01-21 14:29:02 +0530

    Revert "updated to hash router"
    This reverts commit 381e2d263ef665840bc6ddbfb5f791bde930e170.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { About } from "./components/About";
 //import { Cta } from "./components/Cta";
