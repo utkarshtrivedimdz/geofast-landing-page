84d730c6d54e4e61cc50117873af09a066ee3d51
geofast <geofast.collabs@gmail.com>
2025-01-23 03:19:03 +0100

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -62,12 +62,11 @@ export const Team = () => {
   return (
     <section id="team" className="container py-24 sm:py-32">
       <h2 className="text-3xl md:text-4xl font-bold">
-        Geofast Games
+        Geofast Games&nbsp;
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-           Core Team{" "}
+          Core Team
         </span>
       </h2>
-
       <p className="mt-4 mb-10 text-xl text-muted-foreground">
         
       </p>
