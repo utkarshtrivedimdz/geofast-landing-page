4338813e08380478aa93a5cfb43cbbde4be44eca
geofast <geofast.collabs@gmail.com>
2025-01-24 04:35:22 +0100

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -13,12 +13,12 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          Interested in our vision?{" "}
+          TESTInterested in our visionTEST?{" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
           </span>
         </h3>
         <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
-          Drop in your email and we will reach out !
+          TESTDrop in your email and we will reach out !
         </p>
 
         <form
@@ -26,7 +26,7 @@ export const Newsletter = () => {
           onSubmit={handleSubmit}
         >
           <Input
-            placeholder="support@geofastgames.com"
+            placeholder="TEST@geofastgames.com"
             className="bg-muted/50 dark:bg-muted/80 "
             aria-label="email"
           />
