89938a14453415cfd320be7ddaa03d55209a0be0
geofast <geofast.collabs@gmail.com>
2025-01-24 21:39:53 +0100

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -15,35 +15,36 @@ interface TestimonialProps {
 const testimonials: TestimonialProps[] = [
   {
     name: "Футбол Кубок и лига",
+    userName: "",
     comment: "The game is awesome, I’ve been watching you since TT.",
   },
   {
     name: "Kamil Wyszynski",
-    userName: "@flyingbum077",
+    userName: "",
     comment:
       "First person to review this game and I must say, this is brilliant.",
   },
   {
     name: "Pau Robles",
-    userName: "@crazyking",
+    userName: "",
     comment:
       "The game is very good, it would be cool to be able to choose a country and with that, make a Campaign mode, in which you could improve that country, and unlock heart improvements. Keep it up.",
   },
   {
     name: "Daniel Simões",
-    userName: "@hot_jupiter",
+    userName: "",
     comment:
       "Incredible and sensational game, a great pastime. I recommend it for people who like geopolitics and at the same time want to be distracted by something.",
   },
   {
     name: "Servula Pacheco",
-    userName: "@jonathan_carbajal",
+    userName: "",
     comment:
       "Geofast is amazing, congratulations on the incredible work! Keep evolving the game!",
   },
   {
     name: "Jonathan Carbajal",
-    userName: "@jack_dunker",
+    userName: "",
     comment: "Very fun and addictive, I bought the sandbox pass 10 minutes in and I'm having lots of fun.",
   },
 ];
