974a31b08bff3e0af4af7528915d5be9e4219674
geofast <geofast.collabs@gmail.com>
2025-01-24 13:43:16 +0100

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -13,9 +13,8 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          {" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            Contact
+            Contact 
           </span>
           Us
         </h3>
