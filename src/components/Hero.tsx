f36066fc1f050b3e874431860b4deb3b6c950abf
geofast <geofast.collabs@gmail.com>
2025-01-24 04:34:40 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Innovating
+              TEST Innovating
             </span>{" "}
             the future
           </h1>{" "}
@@ -17,18 +17,18 @@ export const Hero = () => {
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
               Mobile
             </span>{" "}
-            gaming
+            gaming TEST
           </h2>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Geofast Games is a cutting-edge game development studio, creating
+          TESTESTEST Geofast Games is a cutting-edge game development studio, creating
           engaging and innovative mobile games. Our latest hit, Battle of
-          Nations, has taken the word game market by storm.
+          Nations, has taken the word game market by storm.TESTESTEST
         </p>
         <a href="#newsletter">
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-full md:w-1/3">Invest In Our Vision</Button>
+            <Button className="w-full md:w-1/3">TESTEST</Button>
           </div>
         </a>
       </div>
