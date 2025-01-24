257ff3495dadeac82ae0da9b923b82762f3e2f66
geofast <geofast.collabs@gmail.com>
2025-01-24 22:11:11 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,13 +9,14 @@ export const Hero = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-start space-y-6">
-        <main className="text-3xl md:text-4xl font-bold">
+        <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               Accessible
             </span>{" "}
             Gaming,
-          </h1>{" "}
+          </h1>
+          <br />
           Unlimited{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
