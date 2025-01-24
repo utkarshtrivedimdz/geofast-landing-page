45b108500163736b9b6128e52f91deddfe84a676
geofast <geofast.collabs@gmail.com>
2025-01-24 13:40:39 +0100

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -13,9 +13,11 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          TESTInterested in our visionTEST?{" "}
+          {" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+            Contact
           </span>
+          Us
         </h3>
         <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
           TESTDrop in your email and we will reach out !
