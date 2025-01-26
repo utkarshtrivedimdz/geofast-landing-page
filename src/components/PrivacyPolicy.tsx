01ff4dc604eea1b7a2c0eee659dd17ac70624512
geofast <geofast.collabs@gmail.com>
2025-01-26 23:43:20 +0100

    Update PrivacyPolicy.tsx

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -11,7 +11,7 @@ const policyData: PolicySectionProps[] = [
     title: "Introduction",
     content: (
       <div>
-        Welcome to Geofast: Battle of Nations, developed by Geofast Games. This
+        Welcome to <em>Geofast: Battle of Nations</em>, developed by Geofast Games. This
         Privacy Policy explains what types of information may be collected when
         you use our game and how this information is handled.
         <br />
