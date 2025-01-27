48c42e131e195b2ff65156b6d50abc88043faa47
geofast <geofast.collabs@gmail.com>
2025-01-27 18:29:19 +0100

    Update ReportTranslationError.tsx

--- src/components/ReportTranslationError.tsx
+++ src/components/ReportTranslationError.tsx
@@ -4,7 +4,7 @@ export const ReportTranslationErrorPage = () => {
   return (
     <GoogleFormIntegration
       formUrl="https://docs.google.com/forms/d/e/1FAIpQLSfRDuTo95xj45FF7t0iH7mR_95RJmcvWqmZv0v4BUT5jDtXgw/viewform"
-      title="Report Translation Error"
+      title="Translation Error Report Form"
       description="We will fix it soon!"
     />
   );
