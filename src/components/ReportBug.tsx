3abe155b8d97db7de76fb4db1faa60d6efc9ff84
geofast <geofast.collabs@gmail.com>
2025-01-27 18:28:25 +0100

    Update ReportBug.tsx

--- src/components/ReportBug.tsx
+++ src/components/ReportBug.tsx
@@ -4,7 +4,7 @@ export const ReportBugPage = () => {
   return (
     <GoogleFormIntegration
       formUrl="https://docs.google.com/forms/d/e/1FAIpQLScNO8Cct6dusU_A1kd0JVeERB2YjFXMQ3GjOmtEq1ojRftgxA/viewform"
-      title="Report Bug Form"
+      title="Bug Report Form"
       description="We will fix it soon!"
     />
   );
